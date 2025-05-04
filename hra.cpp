061909c171bd39aac69f1591fca97fb15e361366
Oospos <Fili.stehlik@gmail.com>
2025-05-04 18:03:26 +0200

    added the first enemy

--- hra.cpp
+++ hra.cpp
@@ -3,11 +3,91 @@
 #include <string>
 using namespace std;
 
+void enemyattack (int maxhp, int maxenergy, int hp, int energy, int enmaxhp, int enhp, int t, int mov [6], int def, int endef, int endmg1, int endmg2, int endmg3, int win, string move1, string move2, string move3) {
+    int x, y;
+    cout << "You encountered an enemy!\n\n";
+    do {
+            do {
+        y=0;
+        if (enhp>0){
+        cout << "Enemy: \n";
+        cout << "Hp: " << enhp << "/" << enmaxhp << endl << endl << endl << endl;
+
+        cout << "You:" << endl;
+        cout << "HP: " << hp << "/" << maxhp << endl;
+        cout << "Energy: " << energy << "/" << maxenergy << endl << endl;
+        cout << "Choose your attack: ";
+        cout << "\n1: " << move1 << "\tdamage - " << mov [0] << "\tenergy use - " << mov [1];
+        cout << "\n2: " << move2 << "\tdamage - " << mov [2] << "\tenergy use - " << mov [3];
+        cout << "\n3: " << move3 << "\tdamage - " << mov [4] << "\tenergy use - " << mov [5] << endl;
+        cin >> t;
+
+
+        switch (t) {
+        case 1:
+            if (energy>=mov [1]){
+            enhp=enhp-mov [0]+endef;
+            energy=energy-mov [1];
+            }
+            break;
+        case 2:
+            if (energy>=mov [3]){
+            enhp=enhp-mov [2]+endef;
+            energy=energy-mov [3];
+            }
+            break;
+        case 3:
+            if (energy>=mov [5]){
+            enhp=enhp-mov [4]+endef;
+            energy=energy-mov [5];
+            }
+            break;
+        default:
+            t=1;
+            cout << "Wrong input!"<<endl<<endl;
+        }
+        }
+            }while(y==1);
+
+
+            if (enhp>0){
+            x=rand() %2;
+            x++;
+            cout << "\n\n\n\n" << x << "\n\n\n\n";
+        switch (x) {
+        case 1:
+            hp=hp-endmg1+def;
+            break;
+        case 2:
+             hp=hp-endmg2+def;
+            break;
+        case 3:
+            hp=hp-endmg3+def;
+            break;
+        default:;}}
+
+
+    }while (enhp>0);
+    cout << "\nEnemy: \n";
+    cout << "Hp: 0/" << enmaxhp << endl << endl << endl << endl;
+
+    cout << "You:" << endl;
+    cout << "HP: " << hp << "/" << maxhp << endl;
+    cout << "Energy: " << energy << "/" << maxenergy << endl << endl << endl << endl;
+if (hp>0){
+    cout << "\n\nYou won!\n\n";
+    win=1;
+}else{
+cout << "You lost...";
+win=0;
+}
+}
+
 
 int main(){
 
     string name, cls, x, move1, move2, move3, move4;
-    int maxhp, maxenergy, hp, energy, gold, level=1, y, meleemult, rangedmult, mov [6];
+    int maxhp, maxenergy, hp, energy, gold, level=1, exp, y, meleemult, rangedmult, mov [6], def, endef, enmaxhp, enhp, t, endmg1, endmg2, endmg3, win;
 
     do{
          cout << "What's your name?\n";
@@ -30,10 +110,10 @@ int main(){
                 y=0;
                 meleemult=1;
                 rangedmult=1;
+                def=5;
 
-                move1="Slam";
-                move2="Parry";
-                move3="Ricoshot";
+                move2="Kick      ";
+                move3="Throw rock";
 
                 mov [0]=15;
                 mov [1]=10;
@@ -57,10 +137,11 @@ int main(){
                 y=0;
                 meleemult=1,5;
                 rangedmult=0,5;
+                def=10;
 
-                move1="Punch";
+                move1="Punch      ";
                 move2="Power blast";
-                move3="Ricoshot";
+                move3="Ricoshot   ";
 
                 mov [0]=10;
                 mov [1]=10;
@@ -84,9 +165,11 @@ int main(){
                 y=0;
                 meleemult=0,5;
                 rangedmult=1.5;
+                def=0;
 
-                move2="Kick";
-                move3="Throw rock";
+                move1="Slam    ";
+                move2="Parry   ";
+                move3="Ricoshot";
 
                 mov [0]=20;
                 mov [1]=15;
@@ -102,7 +185,7 @@ int main(){
                         "\nGold - " << gold;
 
             }else if (cls == "Sigms" ) {
-                maxhp=1000;
+                maxhp=100000;
                 maxenergy=1000;
                 hp=maxhp;
                 energy=maxenergy;
@@ -110,16 +193,17 @@ int main(){
                 y=0;
                 meleemult=3;
                 rangedmult=3;
+                def=1000;
 
-                move1="Touch of death";
-                move2="Laser";
+                move1="Touch of death           ";
+                move2="Laser                    ";
                 move3="Speed of light rock throw";
 
-                mov [0]=10000;
-                mov [1]=10;
+                mov [0]=15000;
+                mov [1]=15;
 
-                mov [2]=15000;
-                mov [3]=15;
+                mov [2]=10000;
+                mov [3]=10;
 
                 mov [4]=5000;
                 mov [5]=5;
@@ -137,9 +221,29 @@ int main(){
 
 
 
+
+    enmaxhp=maxhp;
+    enhp=enmaxhp;
+    endef=0;
+    endmg1=mov[0];
+    endmg2=mov[2];
+    endmg3=mov[4];
+
+    enemyattack (maxhp, maxenergy, hp, energy, enmaxhp, enhp, t, mov, def, endef, endmg1, endmg2, endmg3, win, move1, move2, move3);
+
+    hp=maxhp;
+    energy=maxenergy;
+    if (win>0){
+        gold=gold*1,5;
+        cout << "You wake up in a city and find a bag of gold next to you. ";
+    }else {
+        cout << "You wake up in a city. ";
+    }
+
+
+
     string place;
     int w, lasersword, lasergun, medpac, chargpac;
-cout << "You find yourself in a city. ";
     do {
         cout << "Where would you like to go? (You can open the inventory by typing 'inv' at any time except in combat)\n\n";
         cout <<"- Shop\n- Engineer\n- Medic\n- Leave\n\n";
